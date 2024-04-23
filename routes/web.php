e0d9128a87a7d87cee2355bb4e549daeea128643
next21-iwamura <iwamura@jackroad.co.jp>
2024-04-23 06:49:16 +0000

    【web.php】ルーティング記述ミス修正

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,12 @@ Route::get('/', function () {
 Route::get('/analysis','App\Http\Controllers\AnalysisController@analysis_view');
 Route::POST('/analysis','App\Http\Controllers\AnalysisController@btn_select');
 
-Route::get('/analysis_search','Analysis_searchController@analysis_view');
-Route::POST('/analysis_search','Analysis_searchController@btn_select');
-Route::get('/analysis_test_search','Analysis_test_searchController@analysis_view');
-Route::POST('/analysis_test_search','Analysis_test_searchController@btn_select');
-Route::get('/analysis2_search','Analysis2_searchController@analysis2_view');
-Route::POST('/analysis2_search','Analysis2_searchController@btn_select');
-Route::get('/analysis2_test_search','Analysis2_test_searchController@analysis2_view');
-Route::POST('/analysis2_test_search','Analysis2_test_searchController@btn_select');
+Route::get('/analysis_search','App\Http\Controllers\Analysis_searchController@analysis_search_view');
+Route::POST('/analysis_search','App\Http\Controllers\Analysis_searchController@btn_select');
+Route::get('/analysis_test_search','App\Http\Controllers\Analysis_test_searchController@analysis_search_view');
+Route::POST('/analysis_test_search','App\Http\Controllers\Analysis_test_searchController@btn_select');
+Route::get('/analysis2_search','App\Http\Controllers\Analysis2_searchController@analysis2_search_view');
+Route::POST('/analysis2_search','App\Http\Controllers\Analysis2_searchController@btn_select');
+Route::get('/analysis2_test_search','App\Http\Controllers\Analysis2_test_searchController@analysis2_search_view');
+Route::POST('/analysis2_test_search','App\Http\Controllers\Analysis2_test_searchController@btn_select');
+
