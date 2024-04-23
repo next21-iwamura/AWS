350fbbc5710720f6efc819a9994f7d93635baf84
next21-iwamura <61408692+next21-iwamura@users.noreply.github.com>
2024-04-23 11:06:57 +0900

    Delete routes directory

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/analysis','App\Http\Controllers\AnalysisController@analysis_view');
-Route::POST('/analysis','App\Http\Controllers\AnalysisController@btn_select');
