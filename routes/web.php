37d472e71dcc653096f08faebdef2104433024ea
root <root@ip-10-1-0-240.ap-northeast-1.compute.internal>
2024-04-29 07:38:54 +0000

    「appデイレクトリ」「partsデイレクトリ」「migrationsデイレクトリ」「routesデイレクトリ」「.envファイル」「composer.jsonファイル」「composer.lockファイル」初回登録

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/analysis','App\Http\Controllers\AnalysisController@analysis_view');
+Route::POST('/analysis','App\Http\Controllers\AnalysisController@btn_select');
+
+Route::get('/analysis_search','App\Http\Controllers\Analysis_searchController@analysis_search_view');
+Route::POST('/analysis_search','App\Http\Controllers\Analysis_searchController@btn_select');
+Route::get('/analysis_test_search','App\Http\Controllers\Analysis_test_searchController@analysis_search_view');
+Route::POST('/analysis_test_search','App\Http\Controllers\Analysis_test_searchController@btn_select');
+Route::get('/analysis2_search','App\Http\Controllers\Analysis2_searchController@analysis2_search_view');
+Route::POST('/analysis2_search','App\Http\Controllers\Analysis2_searchController@btn_select');
+Route::get('/analysis2_test_search','App\Http\Controllers\Analysis2_test_searchController@analysis2_search_view');
+Route::POST('/analysis2_test_search','App\Http\Controllers\Analysis2_test_searchController@btn_select');
